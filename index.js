dbb126585cb873129a30dacecf04bedc5d70ccb7
Loffe Knutsson <loffe.knutsson@gmail.com>
2019-12-17 13:06:06 +0100

    JS started

--- index.js
+++ index.js
@@ -1,7 +1,45 @@
 function init(){
-
-  // Här skriver vi kod för att kunna använda oss utav navigations menyn.
+  addEventListeners();
 
 }
+
+function addEventListeners(){
+  document.getElementById("starters").addEventListener("click", starter, true);
+}
+
+function starter(){
+  document.getElementsByTagName("body")[0].innerHTML = "";
+
+  let section = document.createElement("section");
+  document.body.appendChild(section);
+
+  let header = document.createElement("h1");
+  header.textContent = "macka";
+  header.setAttribute("id", "page-header");
+  section.appendChild(header);
+
+  let image = document.createElement("img");
+  image.src = "starters/gin/starter1.jpg";
+  image.setAttribute("style", "height:300px");
+  section.appendChild(image);
+
+  let divPrepareInfo = document.createElement("div");
+  divPrepareInfo.setAttribute("style", "height:50px; background-color:lightgrey");
+  section.appendChild(divPrepareInfo);
+
+  let divIngredients = document.createElement("div");
+  section.appendChild(divIngredients);
+
+  let list = document.createElement("ul");
+  section.appendChild(list);
+
+  let ing1 = document.createElement("li");
+  ing1.textContent = "1 kg sill";
+  list.appendChild(ing1);
+
+
+}
+
+
 
 document.addEventListener("DOMContentLoaded", init);
