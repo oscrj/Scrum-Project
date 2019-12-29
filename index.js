01383c6532276225d45d6ea1506bf1fed725c6e2
Magnus Quist <rydalund@outlook.com>
2019-12-30 00:24:38 +0100

    Update index.js
    Version with the first recipe scalable, the others untouched (to still enable testing)

--- index.js
+++ index.js
@@ -167,7 +167,6 @@ function starter1(){
   document.body.appendChild(section);
 
   let header = document.createElement("header");
-
   header.setAttribute("id", "rec-header");
   section.appendChild(header);
 
@@ -188,10 +187,14 @@ function starter1(){
   let imgTime = document.createElement("img");
   let cookTime = document.createElement("h2");
   let selector = document.createElement("select");
-  let option2 = document.createElement("option");
-  let option4 = document.createElement("option");
-  let option6 = document.createElement("option");
-  let option8 = document.createElement("option");
+  selector.id = "numberOfPortions";
+
+  for(let i = 2; i <= 8; i+=2){
+    let foo =  selector.appendChild(document.createElement("option"));
+    foo.value = i;
+    foo.text = i +" port"; 
+  }
+
   let imgCutlery = document.createElement("img");
   divCookTime.id = "cookTime";
   divCutlery.id = "portions";
@@ -199,21 +202,17 @@ function starter1(){
   imgTime.style.height = "30px";
   imgTime.style.width = "30px";
   cookTime.textContent = "38 min";
-  option2.text = "2 port";
-  option4.text = "4 port";
-  option6.text = "6 port";
-  option8.text = "8 port";
   imgCutlery.src = "media/cutlery.png";
   imgCutlery.style.height = "28px";
   imgCutlery.style.width = "28px";
-  
-  selector.appendChild(option6); // 6 - 8 port
   divPrepareInfo.appendChild(divCookTime);
   divPrepareInfo.appendChild(divCutlery);
   divCookTime.appendChild(imgTime);
   divCookTime.appendChild(cookTime);
   divCutlery.appendChild(imgCutlery);
   divCutlery.appendChild(selector);
+
+  selector.onchange = function(){resizeIngredients(numberOfPortions);false};
 
   let hr = document.createElement("hr");
   section.appendChild(hr);
@@ -223,78 +222,324 @@ function starter1(){
   divIngredients.id = "ingredients";
   section.appendChild(divIngredients);
 
-  let list = document.createElement("ul");
-  divIngredients.appendChild(list);
+  function resizeIngredients(){
+    let foo = parseInt(document.getElementById("numberOfPortions").value);
+    removeIngredients()
+    switch(foo){
+      case 2:
+        twoPortions();
+        break;
+      case 4:
+        fourPortions();
+        break;
+      case 6:
+        sixPortions();
+        break;
+      case 8:
+        eightPortions();
+        break;
+    }
+  }
 
-  let ing1 = document.createElement("li");
-  ing1.textContent = "600 g sushilax";
-  list.appendChild(ing1);
+  function removeIngredients(){
+    document.getElementById("ingredients").innerHTML = "";
+  }
 
-  let ing2 = document.createElement("li");
-  ing2.textContent = "6 enbär";
-  list.appendChild(ing2);
+  function twoPortions(){
+    let list = document.createElement("ul");
+    divIngredients.appendChild(list);
 
-  let ing3 = document.createElement("li");
-  ing3.textContent = "75 g salt";
-  list.appendChild(ing3);
+    let ing1 = document.createElement("li");
+    ing1.textContent = "200 g sushilax";
+    list.appendChild(ing1);
 
-  let ing4 = document.createElement("li");
-  ing4.textContent = "1/2 dl socker";
-  list.appendChild(ing4);
+    let ing2 = document.createElement("li");
+    ing2.textContent = "2 enbär";
+    list.appendChild(ing2);
 
-  let ing5 = document.createElement("li");
-  ing5.textContent = "1/2 dl gin";
-  list.appendChild(ing5);
+    let ing3 = document.createElement("li");
+    ing3.textContent = "13 g salt";
+    list.appendChild(ing3);
 
-  let ing6 = document.createElement("li");
-  ing6.textContent = "1 1/2 dl tonic";
-  list.appendChild(ing6);
+    let ing4 = document.createElement("li");
+    ing4.textContent = "1 msk socker";
+    list.appendChild(ing4);
 
-  let head1 = document.createElement("li");
-  head1.textContent = "Limesås:";
-  head1.setAttribute("class", "listHeader");
-  list.appendChild(head1);
+    let ing5 = document.createElement("li");
+    ing5.textContent = "1 msk gin";
+    list.appendChild(ing5);
 
-  let ing7 = document.createElement("li");
-  ing7.textContent = "1 dl crème fraiche";
-  list.appendChild(ing7);
+    let ing6 = document.createElement("li");
+    ing6.textContent = "1/2 dl tonic";
+    list.appendChild(ing6);
 
-  let ing8 = document.createElement("li");
-  ing8.textContent = "1 lime, saft och rivet skal";
-  list.appendChild(ing8);
+    let head1 = document.createElement("li");
+    head1.textContent = "Limesås:";
+    head1.setAttribute("class", "listHeader");
+    list.appendChild(head1);
 
-  let ing9 = document.createElement("li");
-  ing9.textContent = "1 dl gräddfil";
-  list.appendChild(ing9);
+    let ing7 = document.createElement("li");
+    ing7.textContent = "2 msk crème fraiche";
+    list.appendChild(ing7);
 
-  let ing10 = document.createElement("li");
-  ing10.textContent = "socker";
-  list.appendChild(ing10);
+    let ing8 = document.createElement("li");
+    ing8.textContent = "1/3 lime, saft och rivet skal";
+    list.appendChild(ing8);
 
-  let ing11 = document.createElement("li");
-  ing11.textContent = "salt och nymalen svartpeppar";
-  list.appendChild(ing11);
+    let ing9 = document.createElement("li");
+    ing9.textContent = "2 msk gräddfil";
+    list.appendChild(ing9);
 
-  let head2 = document.createElement("li");
-  head2.textContent = "Inlagd gurka:";
-  head2.setAttribute("class", "listHeader");
-  list.appendChild(head2);
+    let ing10 = document.createElement("li");
+    ing10.textContent = "socker";
+    list.appendChild(ing10);
+    let ing11 = document.createElement("li");
+    ing11.textContent = "salt och nymalen svartpeppar";
+    list.appendChild(ing11);
 
-  let ing12 = document.createElement("li");
-  ing12.textContent = "4 snacksgurkor";
-  list.appendChild(ing12);
+    let head2 = document.createElement("li");
+    head2.textContent = "Inlagd gurka:";
+    head2.setAttribute("class", "listHeader");
+    list.appendChild(head2);
 
-  let ing13 = document.createElement("li");
-  ing13.textContent = "1/2 dl ättika, 12%";
-  list.appendChild(ing13);
+    let ing12 = document.createElement("li");
+    ing12.textContent = "2 snacksgurka";
+    list.appendChild(ing12);
 
-  let ing14 = document.createElement("li");
-  ing14.textContent = "1/2 dl socker";
-  list.appendChild(ing14);
+    let ing13 = document.createElement("li");
+    ing13.textContent = "1 msk ättika, 12%";
+    list.appendChild(ing13);
 
-  let ing15 = document.createElement("li");
-  ing15.textContent = "1/2 dl tonic";
-  list.appendChild(ing15);
+    let ing14 = document.createElement("li");
+    ing14.textContent = "1 msk socker";
+    list.appendChild(ing14);
+
+    let ing15 = document.createElement("li");
+    ing15.textContent = "1 msk tonic";
+    list.appendChild(ing15);
+  }
+
+  function fourPortions(){
+    let list = document.createElement("ul");
+    divIngredients.appendChild(list);
+
+    let ing1 = document.createElement("li");
+    ing1.textContent = "400 g sushilax";
+    list.appendChild(ing1);
+
+    let ing2 = document.createElement("li");
+    ing2.textContent = "4 enbär";
+    list.appendChild(ing2);
+
+    let ing3 = document.createElement("li");
+    ing3.textContent = "25 g salt";
+    list.appendChild(ing3);
+
+    let ing4 = document.createElement("li");
+    ing4.textContent = "2 msk socker";
+    list.appendChild(ing4);
+
+    let ing5 = document.createElement("li");
+    ing5.textContent = "2 msk gin";
+    list.appendChild(ing5);
+
+    let ing6 = document.createElement("li");
+    ing6.textContent = "1 dl tonic";
+    list.appendChild(ing6);
+
+    let head1 = document.createElement("li");
+    head1.textContent = "Limesås:";
+    head1.setAttribute("class", "listHeader");
+    list.appendChild(head1);
+
+    let ing7 = document.createElement("li");
+    ing7.textContent = "4 msk crème fraiche";
+    list.appendChild(ing7);
+
+    let ing8 = document.createElement("li");
+    ing8.textContent = "1/2 lime, saft och rivet skal";
+    list.appendChild(ing8);
+
+    let ing9 = document.createElement("li");
+    ing9.textContent = "4 msk gräddfil";
+    list.appendChild(ing9);
+
+    let ing10 = document.createElement("li");
+    ing10.textContent = "socker";
+    list.appendChild(ing10);
+    let ing11 = document.createElement("li");
+    ing11.textContent = "salt och nymalen svartpeppar";
+    list.appendChild(ing11);
+
+    let head2 = document.createElement("li");
+    head2.textContent = "Inlagd gurka:";
+    head2.setAttribute("class", "listHeader");
+    list.appendChild(head2);
+
+    let ing12 = document.createElement("li");
+    ing12.textContent = "4 snacksgurka";
+    list.appendChild(ing12);
+
+    let ing13 = document.createElement("li");
+    ing13.textContent = "2 msk ättika, 12%";
+    list.appendChild(ing13);
+
+    let ing14 = document.createElement("li");
+    ing14.textContent = "2 msk socker";
+    list.appendChild(ing14);
+
+    let ing15 = document.createElement("li");
+    ing15.textContent = "2 msk tonic";
+    list.appendChild(ing15);
+  }
+
+  function sixPortions(){
+    let list = document.createElement("ul");
+    divIngredients.appendChild(list);
+
+    let ing1 = document.createElement("li");
+    ing1.textContent = "600 g sushilax";
+    list.appendChild(ing1);
+
+    let ing2 = document.createElement("li");
+    ing2.textContent = "6 enbär";
+    list.appendChild(ing2);
+
+    let ing3 = document.createElement("li");
+    ing3.textContent = "75 g salt";
+    list.appendChild(ing3);
+
+    let ing4 = document.createElement("li");
+    ing4.textContent = "1/2 dl socker";
+    list.appendChild(ing4);
+
+    let ing5 = document.createElement("li");
+    ing5.textContent = "1/2 dl gin";
+    list.appendChild(ing5);
+
+    let ing6 = document.createElement("li");
+    ing6.textContent = "1 1/2 dl tonic";
+    list.appendChild(ing6);
+
+    let head1 = document.createElement("li");
+    head1.textContent = "Limesås:";
+    head1.setAttribute("class", "listHeader");
+    list.appendChild(head1);
+
+    let ing7 = document.createElement("li");
+    ing7.textContent = "1 dl crème fraiche";
+    list.appendChild(ing7);
+
+    let ing8 = document.createElement("li");
+    ing8.textContent = "1 lime, saft och rivet skal";
+    list.appendChild(ing8);
+
+    let ing9 = document.createElement("li");
+    ing9.textContent = "1 dl gräddfil";
+    list.appendChild(ing9);
+
+    let ing10 = document.createElement("li");
+    ing10.textContent = "socker";
+    list.appendChild(ing10);
+    let ing11 = document.createElement("li");
+    ing11.textContent = "salt och nymalen svartpeppar";
+    list.appendChild(ing11);
+
+    let head2 = document.createElement("li");
+    head2.textContent = "Inlagd gurka:";
+    head2.setAttribute("class", "listHeader");
+    list.appendChild(head2);
+
+    let ing12 = document.createElement("li");
+    ing12.textContent = "4 snacksgurkor";
+    list.appendChild(ing12);
+
+    let ing13 = document.createElement("li");
+    ing13.textContent = "1/2 dl ättika, 12%";
+    list.appendChild(ing13);
+
+    let ing14 = document.createElement("li");
+    ing14.textContent = "1/2 dl socker";
+    list.appendChild(ing14);
+
+    let ing15 = document.createElement("li");
+    ing15.textContent = "1/2 dl tonic";
+    list.appendChild(ing15);
+  }
+
+  function eightPortions(){
+    let list = document.createElement("ul");
+    divIngredients.appendChild(list);
+
+    let ing1 = document.createElement("li");
+    ing1.textContent = "800 g sushilax";
+    list.appendChild(ing1);
+
+    let ing2 = document.createElement("li");
+    ing2.textContent = "8 enbär";
+    list.appendChild(ing2);
+
+    let ing3 = document.createElement("li");
+    ing3.textContent = "50 g salt";
+    list.appendChild(ing3);
+
+    let ing4 = document.createElement("li");
+    ing4.textContent = "4 msk socker";
+    list.appendChild(ing4);
+
+    let ing5 = document.createElement("li");
+    ing5.textContent = "4 msk gin";
+    list.appendChild(ing5);
+
+    let ing6 = document.createElement("li");
+    ing6.textContent = "2 dl tonic";
+    list.appendChild(ing6);
+
+    let head1 = document.createElement("li");
+    head1.textContent = "Limesås:";
+    head1.setAttribute("class", "listHeader");
+    list.appendChild(head1);
+
+    let ing7 = document.createElement("li");
+    ing7.textContent = "8 msk crème fraiche";
+    list.appendChild(ing7);
+
+    let ing8 = document.createElement("li");
+    ing8.textContent = "1 lime, saft och rivet skal";
+    list.appendChild(ing8);
+
+    let ing9 = document.createElement("li");
+    ing9.textContent = "8 msk gräddfil";
+    list.appendChild(ing9);
+
+    let ing10 = document.createElement("li");
+    ing10.textContent = "socker";
+    list.appendChild(ing10);
+    let ing11 = document.createElement("li");
+    ing11.textContent = "salt och nymalen svartpeppar";
+    list.appendChild(ing11);
+
+    let head2 = document.createElement("li");
+    head2.textContent = "Inlagd gurka:";
+    head2.setAttribute("class", "listHeader");
+    list.appendChild(head2);
+
+    let ing12 = document.createElement("li");
+    ing12.textContent = "8 snacksgurka";
+    list.appendChild(ing12);
+
+    let ing13 = document.createElement("li");
+    ing13.textContent = "4 msk ättika, 12%";
+    list.appendChild(ing13);
+
+    let ing14 = document.createElement("li");
+    ing14.textContent = "4 msk socker";
+    list.appendChild(ing14);
+
+    let ing15 = document.createElement("li");
+    ing15.textContent = "4 msk tonic";
+    list.appendChild(ing15);
+  }
 
   let divDescription = document.createElement("div");
   divDescription.setAttribute("class", "container");
@@ -330,7 +575,6 @@ function starter1(){
   let descr2 = document.createElement("li");
   descr2.textContent = "Smaka av med socker, salt och peppar.";
 
-
   descriptionList2.appendChild(descr1);
   descriptionList2.appendChild(descr2);
 
@@ -348,15 +592,14 @@ function starter1(){
   let descri3 = document.createElement("li");
   descri3.textContent = "Ta upp laxfilén ur lagen, klappa torr med hushållspapper. Skär i tunna skivor och lägg upp på tallrik tillsammans med inlagd gurka, limesås, rostat rågbröd och örter.";
 
-
   descriptionList3.appendChild(descri1);
   descriptionList3.appendChild(descri2);
   descriptionList3.appendChild(descri3);
 
+  twoPortions();
   footer();
   btnBackOneStep();
   btnsLikeDislike();
-
 }
 
 /**Start2 - sill */
